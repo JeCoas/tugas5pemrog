032845e6eeffaf51b5e69ad25a7e5d570f759755
JeCoas <127827888+JeCoas@users.noreply.github.com>
2023-04-10 20:16:00 +0700

    Add files via upload

---
+++ MinMax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._11._4856
+{
+    class MinMax
+    {
+        public void GetMinMax(int[] arr, out int max, out int min)
+        {
+            max = arr[0];
+            min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using _22._11._4856;
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 90, 72, 41, 21, 93, 35 };
+
+            MinMax mm = new MinMax();
+
+            int max, min;
+            mm.GetMinMax(arr, out max, out min);
+
+            Console.WriteLine("Bilangan terbesar: " + max);
+            Console.WriteLine("Bilangan terkecil: " + min);
+        }
+    }
+}
+
